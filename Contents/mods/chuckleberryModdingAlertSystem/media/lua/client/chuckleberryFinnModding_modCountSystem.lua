21ea5bcc46df2f84301e95b67495303f21914889
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-14 18:51:53 -0500

    Update chuckleberryFinnModding_modCountSystem.lua

--- Contents/mods/chuckleberryModdingAlertSystem/media/lua/client/chuckleberryFinnModding_modCountSystem.lua
+++ Contents/mods/chuckleberryModdingAlertSystem/media/lua/client/chuckleberryFinnModding_modCountSystem.lua
@@ -26,6 +26,7 @@ function modCountSystem.pullCurrentFileModInfo()
     local coroutine = getCurrentCoroutine()
     local count = coroutine and getCallframeTop(coroutine)
 
+    ---@type ChooseGameInfo.Mod
     local modInfo
     for i= count - 1, 0, -1 do
         ---@type LuaCallFrame
@@ -48,8 +49,7 @@ function modCountSystem.addModID(modID) modCountSystem.workshopIDs[modID] = true
 
 function modCountSystem.pullAndAddModID()
     modCountSystem.assembleModIDs()
-    return
-    --[[
+
     ---@type ChooseGameInfo.Mod
     local modInfo = modCountSystem.pullCurrentFileModInfo()
     if not modInfo then return end
@@ -63,7 +63,6 @@ function modCountSystem.pullAndAddModID()
     modCountSystem.workshopIDs[workshopID] = modInfo
 
     modCountSystem.count = modCountSystem.count+1
-    --]]
 end
 
 
